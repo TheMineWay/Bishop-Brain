dcaed7ff6140f7402b9f6606707c17b38119d938
Joel Campos <joelcamposoliva9000@gmail.com>
2024-01-06 12:07:45 +0100

    Setup NextUI provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.pcss";
 import App from "./app";
+import Providers from "./providers/providers";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );
