5596b46d82d9fd0e0fe5752ea269dd3196369271
Joel Campos <joelcamposoliva9000@gmail.com>
2024-01-06 15:04:34 +0100

    Translate aria-label

--- src/components/common/navbar/navbar.tsx
+++ src/components/common/navbar/navbar.tsx
@@ -5,15 +5,23 @@ import {
   NavbarBrand,
 } from "@nextui-org/react";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Translation } from "../../../i18n/translation.enum";
 
 export default function Navbar() {
+  const { t } = useTranslation([Translation.LAYOUT]);
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <NextNavbar onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent>
         <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          aria-label={
+            isMenuOpen
+              ? t("navbar.actions.Close-menu")
+              : t("navbar.actions.Open-menu")
+          }
           className="sm:hidden"
         />
         <NavbarBrand>
