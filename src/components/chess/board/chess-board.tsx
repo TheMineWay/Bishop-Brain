683e27d09749e8748c654b177988b44d387565ef
Joel Campos <joelcamposoliva9000@gmail.com>
2024-01-07 11:36:02 +0100

    Chess board basic UI

--- src/components/chess/board/chess-board.tsx
+++ src/components/chess/board/chess-board.tsx
@@ -1,10 +1,31 @@
 import { ChessBoardController } from "../../../services/chess/board/chess-board.controller";
 
+import styles from "./chess-board.module.pcss";
+
 type Props = {
   controller: ChessBoardController;
 };
 
+const WHITE = "white",
+  BLACK = "black";
+
 // eslint-disable-next-line no-empty-pattern
 export default function ChessBoard({}: Props) {
-  return <div>BOARD</div>;
+  return (
+    <div className={styles.board}>
+      {[...new Array(8)].map((_, i) => (
+        <div key={i} className={styles.row}>
+          {[...new Array(8)].map((_, j) => (
+            <div
+              key={j}
+              className={styles.cell}
+              style={{ backgroundColor: (i + j) % 2 === 0 ? WHITE : BLACK }}
+            >
+              <div>{j}</div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
