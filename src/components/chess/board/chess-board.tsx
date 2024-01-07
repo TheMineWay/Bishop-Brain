d375d46de9351b5489c32b5d20cfb3e75a60713f
Joel Campos <joelcamposoliva9000@gmail.com>
2024-01-07 11:54:01 +0100

    Paint cells

--- src/components/chess/board/chess-board.tsx
+++ src/components/chess/board/chess-board.tsx
@@ -11,21 +11,45 @@ const WHITE = "white",
 
 // eslint-disable-next-line no-empty-pattern
 export default function ChessBoard({}: Props) {
+  const facingWhite = true;
+
+  const ABC = "abcdefgh".split("")[facingWhite ? "slice" : "reverse"]();
+  const NUMS = [1, 2, 3, 4, 5, 6, 7, 8][facingWhite ? "reverse" : "slice"]();
+
   return (
     <div className={styles.board}>
-      {[...new Array(8)].map((_, i) => (
-        <div key={i} className={styles.row}>
-          {[...new Array(8)].map((_, j) => (
-            <div
-              key={j}
-              className={styles.cell}
-              style={{ backgroundColor: (i + j) % 2 === 0 ? WHITE : BLACK }}
-            >
-              <div>{j}</div>
-            </div>
-          ))}
-        </div>
-      ))}
+      {[...new Array(8)].map((_, r) => {
+        const row = NUMS[r];
+
+        return (
+          <div key={r} className={styles.row}>
+            {[...new Array(8)].map((_, c) => {
+              const cell = ABC[c];
+
+              return (
+                <div
+                  key={c}
+                  className={styles.cell}
+                  style={{
+                    backgroundColor:
+                      (NUMS.findIndex((v) => v === row) +
+                        ABC.findIndex((v) => v === cell)) %
+                        2 ===
+                      0
+                        ? WHITE
+                        : BLACK,
+                  }}
+                >
+                  <div>
+                    {cell}
+                    {row}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
